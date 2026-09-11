e24be1ffaeb6bca5af01ebe25cae398bbbcfc719
Amruta4551 <amrutagaikwad11322@gmail.com>
2026-09-11 17:56:29 +0530

    Added string_formatting.py

---
+++ string_formatting.py
@@ -0,0 +1,12 @@
+def print_formatted(number):
+    width = len(bin(number))-2
+    for i in range(1,number+1):
+        d = str(i).rjust(width)
+        o = oct(i)[2:].rjust(width)
+        x = hex(i)[2:].rjust(width).upper()
+        b = bin(i)[2:].rjust(width)
+        print(d, o, x, b)
+
+if __name__ == '__main__':
+    n = int(input())
+    print_formatted(n)
